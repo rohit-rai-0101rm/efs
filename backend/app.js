5376c6324ca62266a0d5e621dac12475c5d73b93
rohit rai <rohitrai.dev@gmail.com>
2021-12-30 23:34:36 +0530

    order routes done ..starting frontend

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,10 @@ app.use(express.json())
 app.use(cookieParser())
 import productRoute from './routes/productRoute.js'
 import userRoute from './routes/userRoute.js'
+import orderRoute from './routes/orderRoute.js'
 app.use("/api/v1",productRoute)
 app.use("/api/v1",userRoute)
-
+app.use("/api/v1",orderRoute)
 app.use(errorMiddleware)
 
 export default app
