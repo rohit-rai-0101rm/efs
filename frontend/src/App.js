70725628c506c67c02c962b7782d6877088088dd
rohit rai <rohitrai.dev@gmail.com>
2021-12-31 21:48:11 +0530

    footer/header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from 'webfontloader'
+import Header from "./components/layout/Header/Header.js";
+import { useEffect } from "react";
+import Footer from "./components/layout/Footer/Footer";
 
-import Header from "./components/layout/Header.js";
 function App() {
+  useEffect(()=>{
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  
+  
+  },[])
   return <Router>
-    <Header/> 
+    <Header/>  
+    <Footer/>
   </Router>;
 }
 
