40261751ab37ced00ae7aab20f53ee02861bb3cc
rohit rai <rohitrai.dev@gmail.com>
2021-12-31 21:32:39 +0530

    overlay-navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import Header from "./components/layout/Header.js";
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return <Router>
+    <Header/> 
+  </Router>;
 }
 
 export default App;
