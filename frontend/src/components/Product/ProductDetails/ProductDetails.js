230133be447182d280c4f02f074aa6e2425c2378
rohit rai <rohitrai.dev@gmail.com>
2022-01-01 15:51:29 +0530

    product details page rendered

--- frontend/src/components/Product/ProductDetails/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails/ProductDetails.js
@@ -1,37 +1,139 @@
-import React, { Fragment ,useEffect} from 'react'
+import React, { Fragment ,useEffect, useState} from 'react'
 import Carousel from "react-material-ui-carousel";
-
+import { Rating } from "@material-ui/lab";
+import { useAlert } from "react-alert";
 import './ProductDetails.css'
 import { useSelector,useDispatch } from 'react-redux'
 
-import { getProductDetails } from '../../../actions/productActions'
+import { clearErrors, getProductDetails } from '../../../actions/productActions'
+import ReviewCard from '../ReviewCard';
+import Loader from '../../layout/Loading/Loading';
+import MetaData from '../../layout/MetaData';
 const ProductDetails = ({match}) => {
-    const dispatch = useDispatch()
-    const {product,loading,error}=useSelector(state=>state.productDetails)
-    useEffect(()=>{
-        dispatch(getProductDetails(match.params.id))
-    },[dispatch,match.params.id])
-    return (
-       <Fragment>
-           <div className='ProductDetails'>
-            <div>
-                <Carousel>
-                    {product.images&&
-                    product.images.map((item,i)=>(
-                        <img
-                        className="CarouselImage"
-                        key={i}
-                        src={item.url}
-                        alt={`${i} Slide`}
-                      />
-                    )
-                    )}
-                </Carousel>
+    const [quantity, setQuantity] = useState(1);
+    const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
 
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (1 >= quantity) return;
+
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+
+    
+    dispatch(getProductDetails(match.params.id));
+  }, [dispatch, match.params.id, error, alert]);
+    const options = {
+        size: "large",
+        value: product.ratings,
+        readOnly: true,
+        precision: 0.5,
+      };
+    return (
+        <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${product.name} -- ECOMMERCE`} />
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
             </div>
-           </div>
-       </Fragment>
-    )
-}
+
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <Rating {...options} />
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`₹${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                <div className="detailsBlock-3-1-1">
+                    <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly type="number" value={quantity} />
+                    <button onClick={increaseQuantity}>+</button>
+                  </div>
+                  <button
+                    
+                    disabled={product.Stock < 1 ? true : false}
+                   
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+
+              <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+
+              <button  className="submitReview">
+                Submit Review
+              </button>
+            </div>
+          </div>
+
+          
+
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
+            </div>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+    
 
 export default ProductDetails
