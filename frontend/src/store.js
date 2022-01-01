22db9bc57e98a70a4141f006acdfd3eae89acdf7
rohit rai <rohitrai.dev@gmail.com>
2022-01-01 20:54:45 +0530

    user reducer added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,13 @@ import { createStore,combineReducers,applyMiddleware} from "redux"
 import thunk from "redux-thunk"
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productDetailsReducer, productReducer } from "./reducers/productReducer";
+import { userReduce } from "./reducers/userReducer";
 const reducer=combineReducers({
     products:productReducer,
-    productDetails:productDetailsReducer
+    productDetails:productDetailsReducer,
+    user:userReduce
+
+
 })
 let initailState={};
 const middleware=[thunk]
